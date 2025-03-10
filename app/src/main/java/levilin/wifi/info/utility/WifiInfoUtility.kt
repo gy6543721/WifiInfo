956a8e771f728e68481cb3909a8166de3aca672d
Lin <gy6543721@yahoo.com.tw>
2025-03-10 13:01:36 +0900

    [Update] Modify data

--- app/src/main/java/levilin/wifi/info/utility/WifiInfoUtility.kt
+++ app/src/main/java/levilin/wifi/info/utility/WifiInfoUtility.kt
@@ -3,20 +3,22 @@ package levilin.wifi.info.utility
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.net.wifi.ScanResult.WIFI_STANDARD_11AC
+import android.net.wifi.ScanResult.WIFI_STANDARD_11AD
 import android.net.wifi.ScanResult.WIFI_STANDARD_11AX
+import android.net.wifi.ScanResult.WIFI_STANDARD_11BE
 import android.net.wifi.ScanResult.WIFI_STANDARD_11N
-import android.net.wifi.WifiConfiguration
+import android.net.wifi.ScanResult.WIFI_STANDARD_LEGACY
 import android.net.wifi.WifiInfo
 import android.net.wifi.WifiManager
 import android.os.Build
-import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.annotation.RequiresApi
 import levilin.wifi.info.ui.model.WifiInfoData
 
 class WifiInfoUtility(private val context: Context) {
+    @RequiresApi(Build.VERSION_CODES.S)
     @SuppressLint("DefaultLocale")
     fun getWifiInfo(): WifiInfoData {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
@@ -26,10 +28,10 @@ class WifiInfoUtility(private val context: Context) {
         val wifiInfo: WifiInfo? = wifiManager.connectionInfo
 
         val networkType = when {
-            networkCapabilities == null -> "No Network"
-            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> "Wi-Fi"
-            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> "Cellular"
-            else -> "Unknown"
+            networkCapabilities == null -> "ネットワークなし"
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> "WiFi回線"
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> "携帯回線"
+            else -> "不明"
         }
 
         return WifiInfoData(
@@ -49,13 +51,16 @@ class WifiInfoUtility(private val context: Context) {
             linkSpeed = wifiInfo?.linkSpeed,
             phyMode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
                 when (wifiInfo?.wifiStandard) {
-                    WIFI_STANDARD_11AC -> "802.11ac"
-                    WIFI_STANDARD_11AX -> "802.11ax"
-                    WIFI_STANDARD_11N -> "802.11n"
-                    else -> "Unknown"
+                    WIFI_STANDARD_LEGACY -> "802.11a/b/g"
+                    WIFI_STANDARD_11N -> "802.11n（WiFi-4・2.4/5GHz）"
+                    WIFI_STANDARD_11AC -> "802.11ac（WiFi-5・5GHz）"
+                    WIFI_STANDARD_11AX -> "802.11ax（WiFi-6・2.4/5/6GHz）"
+                    WIFI_STANDARD_11AD -> "802.11ad（WiGig・60GHZ）"
+                    WIFI_STANDARD_11BE -> "802.11be（WiFi-7・2.4/5/6GHz）"
+                    else -> "不明"
                 }
             } else {
-                "Unknown"
+                "不明"
             },
             channel = wifiInfo?.frequency?.let { freq ->
                 when {
@@ -76,24 +81,28 @@ class WifiInfoUtility(private val context: Context) {
             } else {
                 null
             },
-            securityType = if (checkSelfPermission(
-                    context,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                wifiManager.configuredNetworks?.firstOrNull { it.networkId == wifiInfo?.networkId }
-                    ?.let { config ->
-                        when {
-                            config.allowedKeyManagement.get(WifiConfiguration.KeyMgmt.WPA_PSK) -> "WPA"
-                            config.allowedKeyManagement.get(WifiConfiguration.KeyMgmt.WPA_EAP) -> "WPA-EAP"
-                            config.allowedKeyManagement.get(WifiConfiguration.KeyMgmt.IEEE8021X) -> "802.1X"
-                            else -> "Open"
-                        }
-                    }
-            } else {
-                "No Permission"
-            },
+            securityType = convertSecurityType(wifiInfo?.currentSecurityType),
+            frequency = wifiInfo?.frequency,
             networkType = networkType
         )
+    }
+
+    fun convertSecurityType(currentSecurityType: Int?): String? {
+        return when(currentSecurityType) {
+            1 -> "WEP"
+            2 -> "PSK"
+            3 -> "EAP"
+            4 -> "SAE"
+            5 -> "EAP_WPA3 ENTERPRISE 192 BIT"
+            6 -> "OWE"
+            7 -> "WAPI_PSK"
+            8 -> "WAPI_CERT"
+            9 -> "EAP_WPA3 ENTERPRISE"
+            10 -> "OSEN"
+            11 -> "PASSPOINT_R1_R2"
+            12 -> "PASSPOINT_R3"
+            13 -> "DPP"
+            else -> "不明"
+        }
     }
 }
